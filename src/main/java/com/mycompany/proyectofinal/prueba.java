22a0c731eb4d38654e1f3a258cd7e0989737d940
LuisAlvarado64 <laalvarado064@gmail.com>
2022-12-05 08:30:33 -0800

    Ya sirve?

--- src/main/java/com/mycompany/proyectofinal/prueba.java
+++ src/main/java/com/mycompany/proyectofinal/prueba.java
@@ -1,8 +1,3 @@
-//<<<<<<< HEAD
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package com.mycompany.proyectofinal;
 
 /**
